11b84b90c0472939869ce2b43624c400536047ab
Fabiano <nbscbs@gmail.com>
2021-11-11 21:58:53 -0300

    Config etec

--- js/index.js
+++ js/index.js
@@ -5,5 +5,13 @@ function trocarimg(filename, animalname){
 }
 function pegarAnimal(){
     let animal=document.querySelector('.img').getAttribute('data-animal');
-    alert('O animal é: '+animal);
+    Swal.fire({
+        position: 'top-end',
+        icon: 'success',
+        title: 'O animal é um GATO' + animal,
+        showConfirmButton: false,
+        timer: 1500
+      } + animal)
+    //swal(" O animal é um GATO", "error");
+    //alert('O animal é: '+animal);
 }
